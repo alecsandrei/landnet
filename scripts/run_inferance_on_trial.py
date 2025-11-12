bfab1ee7a11ce88148d186f93356dd2ecd11be2d
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-11-12 10:14:44 +0200

    update run_inferance_on_trial.py

--- scripts/run_inferance_on_trial.py
+++ scripts/run_inferance_on_trial.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 from ray.train import Result
 
-from landnet.config import MODELS_DIR, TRIAL_NAME
 from landnet.enums import GeomorphometricalVariable
 from landnet.features.tiles import TileConfig, TileSize
 from landnet.modelling.classification.inference import InferTrainTest
@@ -12,49 +11,25 @@ from landnet.modelling.tune import MetricSorter
 from landnet.typing import TuneSpace
 
 if __name__ == '__main__':
+    sorter = MetricSorter('val_f2_score', 'max')
     infer = InferTrainTest(
         variables=[
-            # GeomorphometricalVariable('area'),
-            # GeomorphometricalVariable('cbl'),
-            # GeomorphometricalVariable('ccros'),
-            # GeomorphometricalVariable('cdl'),
-            # GeomorphometricalVariable('cdo'),
-            # GeomorphometricalVariable('cgene'),
-            GeomorphometricalVariable('clong'),
-            # GeomorphometricalVariable('clo'),
-            # GeomorphometricalVariable('clu'),
-            # GeomorphometricalVariable('cmaxi'),
-            GeomorphometricalVariable('cmini'),
-            GeomorphometricalVariable('conv'),
-            GeomorphometricalVariable('cplan'),
-            # GeomorphometricalVariable('nego'),
-            # GeomorphometricalVariable('northness'),
-            # GeomorphometricalVariable('poso'),
-            # GeomorphometricalVariable('shade'),
-            # GeomorphometricalVariable('slope'),
-            GeomorphometricalVariable('cprof'),
-            # GeomorphometricalVariable('croto'),
-            # GeomorphometricalVariable('ctang'),
-            # GeomorphometricalVariable('cup'),
-            GeomorphometricalVariable('dem'),
-            # GeomorphometricalVariable('eastness'),
-            GeomorphometricalVariable('tpi'),
-            GeomorphometricalVariable('tri'),
-            GeomorphometricalVariable('twi'),
-            # GeomorphometricalVariable('wind'),
-            # GeomorphometricalVariable('vld'),
+            GeomorphometricalVariable.HILLSHADE,
+            GeomorphometricalVariable.TOPOGRAPHIC_POSITION_INDEX,
+            GeomorphometricalVariable.NEGATIVE_TOPOGRAPHIC_OPENNESS,
+            GeomorphometricalVariable.DIGITAL_ELEVATION_MODEL,
+            GeomorphometricalVariable.TERRAIN_RUGGEDNESS_INDEX,
         ],
-        sorter=MetricSorter('val_f_beta', 'max'),
         out_dir=None,
     )
-    for trial in (
-        MODELS_DIR / TRIAL_NAME / TRIAL_NAME / '2025-04-24_08-13-50'
-    ).iterdir():
+    trial_path = Path(
+        '/media/alex/alex/python-modules-packages-utils/landnet/models/convnext_100x100_5vars/convnext_100x100_5vars/2025-06-27_16-46-08'
+    )
+    for trial in trial_path.iterdir():
         if not trial.is_dir() or (trial / 'predictions').exists():
             continue
         try:
             ...
-            # trial = Trial.from_directory(trial)
         except FileNotFoundError:
             continue
         if not any(
@@ -62,7 +37,7 @@ if __name__ == '__main__':
         ):
             continue
         best_checkpoint = Result.from_path(trial).get_best_checkpoint(
-            infer.sorter.metric, infer.sorter.mode
+            sorter.metric, sorter.mode
         )
         tune_space: TuneSpace = {
             'batch_size': 1,
