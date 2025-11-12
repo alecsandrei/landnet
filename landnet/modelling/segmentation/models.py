204a7522bf936b39896544ac79b8817e7d3cf656
Alex Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2025-11-12 09:59:43 +0200

    switch to py3.12 generics syntax and better model io adapt

--- landnet/modelling/segmentation/models.py
+++ landnet/modelling/segmentation/models.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import collections.abc as c
-import typing as t
 
 import segmentation_models_pytorch as smp
 from torch import hub, nn
@@ -18,8 +17,6 @@ from landnet.config import PRETRAINED
 from landnet.enums import Architecture, Mode
 from landnet.modelling.models import ModelBuilder
 
-M = t.TypeVar('M', bound=nn.Module)
-
 
 def get_architecture(
     architecture: Architecture,
@@ -27,7 +24,7 @@ def get_architecture(
     return MODEL_BUILDERS[architecture].build
 
 
-class DeepLabV3ResNet50Builder(ModelBuilder):
+class DeepLabV3ResNet50Builder(ModelBuilder[DeepLabV3]):
     def __init__(self):
         # we do not provide num_classes argument here because it gets
         # overriden if weights are specified (check source code of torchvision)
@@ -36,36 +33,46 @@ class DeepLabV3ResNet50Builder(ModelBuilder):
             weights=DeepLabV3_ResNet50_Weights.DEFAULT if PRETRAINED else None,
         )
 
-    def _adapt_output_features(self, model: M) -> M:
-        if isinstance(model, nn.Sequential):
-            deeplabv3 = model[1]
-        else:
-            deeplabv3 = model
-        deeplabv3 = t.cast(DeepLabV3, deeplabv3)
-        assert isinstance(deeplabv3.classifier, nn.Sequential)
-        deeplabv3.classifier[-1] = nn.Conv2d(
-            256, 2, kernel_size=(1, 1), stride=(1, 1)
+    def _adapt_output_features(self, model: DeepLabV3) -> DeepLabV3:
+        current = model.classifier[-1]
+        new = nn.Conv2d(
+            current.in_channels,
+            out_channels=2,
+            kernel_size=current.kernel_size,
+            stride=current.stride,
+            padding=current.padding,
+            dilation=current.dilation,
+            groups=current.groups,
+            bias=True,
+            padding_mode=current.padding_mode,
         )
+
+        model.classifier[-1] = new
         return model
 
 
-class FCNResNet50Builder(ModelBuilder):
+class FCNResNet50Builder(ModelBuilder[FCN]):
     def __init__(self):
         super().__init__(
             model=fcn_resnet50,
             weights=FCN_ResNet50_Weights.DEFAULT if PRETRAINED else None,
         )
 
-    def _adapt_output_features(self, model: M) -> M:
-        if isinstance(model, nn.Sequential):
-            fcn = model[1]
-        else:
-            fcn = model
-        fcn = t.cast(FCN, fcn)
-        assert isinstance(fcn.classifier, nn.Sequential)
-        fcn.classifier[-1] = nn.Conv2d(
-            512, 2, kernel_size=(1, 1), stride=(1, 1)
+    def _adapt_output_features(self, model: FCN) -> FCN:
+        current = model.classifier[-1]
+        new = nn.Conv2d(
+            current.in_channels,
+            out_channels=2,
+            kernel_size=current.kernel_size,
+            stride=current.stride,
+            padding=current.padding,
+            dilation=current.dilation,
+            groups=current.groups,
+            bias=True,
+            padding_mode=current.padding_mode,
         )
+
+        model.classifier[-1] = new
         return model
 
 
@@ -78,7 +85,7 @@ def get_unet_model_milesial():
     )
 
 
-def get_unet_model_smp(in_channels: int, classes: int):
+def get_unet_model_smp(in_channels: int, classes: int) -> smp.Unet:
     return smp.Unet(
         encoder_name='resnet101',
         in_channels=in_channels,
@@ -87,31 +94,34 @@ def get_unet_model_smp(in_channels: int, classes: int):
     )
 
 
-class UNetBuilder(ModelBuilder):
-    def __init__(self, in_channels: int, classes: int):
-        super().__init__(
-            model=lambda: get_unet_model_smp(
-                in_channels=in_channels, classes=classes
-            ),
-            weights=None,
-        )
+class UNetBuilder(ModelBuilder[smp.Unet]):
+    def __init__(self):
+        super().__init__(model=get_unet_model_smp, weights=None)
 
-    def _adapt_output_features(self, model: M) -> M:
-        # breakpoint()
-        # if isinstance(model, nn.Sequential):
-        #     fcn = model[1]
-        # else:
-        #     fcn = model
-        # fcn = t.cast(FCN, fcn)
-        # assert isinstance(fcn.classifier, nn.Sequential)
-        # fcn.classifier[-1] = nn.Conv2d(
-        #     512, 2, kernel_size=(1, 1), stride=(1, 1)
-        # )
+    def _adapt_output_features(self, model: smp.Unet) -> smp.Unet:
+        return model
+
+    def _adapt_input_channels(
+        self, model: smp.Unet, in_channels: int
+    ) -> smp.Unet:
+        current = model.encoder.conv1
+        new = nn.Conv2d(
+            in_channels,
+            out_channels=current.out_channels,
+            kernel_size=current.kernel_size,
+            stride=current.stride,
+            padding=current.padding,
+            dilation=current.dilation,
+            groups=current.groups,
+            bias=True,
+            padding_mode=current.padding_mode,
+        )
+        model.classifier[-1] = new
         return model
 
 
 MODEL_BUILDERS: dict[Architecture, ModelBuilder] = {
     Architecture.DEEPLABV3: DeepLabV3ResNet50Builder(),
     Architecture.FCN: FCNResNet50Builder(),
-    # Architecture.UNET: UNetBuilder(),
+    Architecture.UNET: UNetBuilder(),
 }
